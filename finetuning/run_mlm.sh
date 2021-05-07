8a88f2ab4448bb9b1abe199e25c5e5a7f3cd759a
Jeremiah <jeremiah.mcrey@gmail.com>
2021-05-06 23:22:12 -0400

    Finished experiments, adding partial report.

--- finetuning/run_mlm.sh
+++ finetuning/run_mlm.sh
@@ -1,8 +1,8 @@
 export N=0
 export CUDA_VISIBLE_DEVICES=$N
 export TRAIN_FILE=$HOME/Documents/cs584-final-project/data/fever_train.txt
-export VALIDATION_FILE=$HOME/Documents/cs584-final-project/data/fever_validation.txt
-export OUTPUT_DIR=/run/media/jmack/ColdStorage/models/bart_large
+export MODEL_DIR=/run/media/jmack/ColdStorage/models/bart_large
+export OUTPUT_DIR=/run/media/jmack/ColdStorage/models/bart_large_dev
 
 echo "Model train file $TRAIN_FILE"
 echo "Model validation file $VALIDATION_FILE"
@@ -11,12 +11,9 @@ sleep 3
 
 python run_mlm.py \
     --output_dir $OUTPUT_DIR \
-    --model_name_or_path facebook/bart-large \
+    --model_name_or_path $MODEL_DIR \
     --train_file $TRAIN_FILE \
-    --validation_file $VALIDATION_FILE \
     --do_train \
-    --do_eval \
     --line_by_line \
     --per_device_train_batch_size 5 \
-    --per_device_eval_batch_size 5 \
-    --max_train_samples 100000
+    --per_device_eval_batch_size 5
